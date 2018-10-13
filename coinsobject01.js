926e67d5e6fc27c8af00332cf7138e24e148fa46
Melissa <melissapotter.student@careerdevs.com>
2018-10-13 14:43:59 +0000

    coin objects exercise

---
+++ coinsobject01.js
@@ -0,0 +1,10 @@
+var assignment="create an object for coins";
+
+var currencyAbbr=['AUD', 'GBP', 'EUR', 'JPY', 'CHF', 'USD', 'AFN', 'ALL', 'DZD', 'AOA'];
+
+var USD={penny:0.01,
+nickel:0.05,
+dime:0.1,
+quarter:0.25,
+halfDollar:0.5,
+dollar:1};
