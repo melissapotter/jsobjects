69492c7ba5adfaa5bf08afb719b9443f58bb0025
Melissa <melissapotter.student@careerdevs.com>
2018-11-19 23:14:18 +0000

    added new method

--- methods/hotelobjectsandmethods.js
+++ methods/hotelobjectsandmethods.js
@@ -1,5 +1,5 @@
-var hotel = {
-    name: "CareerDevs Hotel",
+var hotel = { //object
+    name: "CareerDevs Hotel", //name: is considered a key, careerdevs hotel is a property
     rating: 5.0,
     roomRate: 325.00,
     roomNumbersAvailable: ["102", "103", "104", "105", "106" ], 
@@ -7,7 +7,7 @@ var hotel = {
     roomType: "Queen",
     
     numberOfRoomsAvailable: function() { 
-     // length of the number of rooms available
+     // length of the number of rooms available, returned as an integer
     return this.roomNumbersAvailable.length;
     }, 
     
@@ -19,6 +19,10 @@ var hotel = {
         // avail rooms plus booked rooms 
         return this.roomNumbersAvailable.length + this.roomNumbersBooked.length;
     },
+    
+    bookRoom: function () {
+        //only book a room if one or more is available
+    }
     
     
 };
