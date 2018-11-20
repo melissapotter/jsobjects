d66b2a915c132684efadd8ba62f11eeaa2ca3503
Melissa <melissapotter.student@careerdevs.com>
2018-11-20 22:44:05 +0000

    added push to booked rooms

--- methods/hotelobjectsandmethods.js
+++ methods/hotelobjectsandmethods.js
@@ -2,8 +2,8 @@ var hotel = { //object
     name: "CareerDevs Hotel", //name: is considered a key, careerdevs hotel is a property
     rating: 5.0,
     roomRate: 325.00,
-    roomNumbersAvailable: ["102", "103", "104", "105", "106" ], 
-    roomNumbersBooked: ["101"],
+    roomNumbersAvailable: ["101", "102", "103", "104", "105", "106" ], 
+    roomNumbersBooked: [ ],
     roomType: "Queen",
     
     numberOfRoomsAvailable: function() { 
@@ -22,7 +22,16 @@ var hotel = { //object
     
     bookRoom: function () {
         //only book a room if one or more is available
-    }
+        if (this.numberOfRoomsAvailable() > 0) {
+            // select a random available room
+            var randomRoom = this.roomNumbersAvailable[Math.floor(Math.random()*this.roomNumbersAvailable.length)];
+            //return available room;
+            //list.splice(list.indexOf('foo'), 1);
+            this.roomNumbersAvailable.splice(this.roomNumbersAvailable.indexOf(randomRoom), 1);
+            //remove the booked room from roomNumbersAvailable and add it to roomNumbersBooked
+            this.roomNumbersBooked.push(randomRoom);
+        }
+    },
     
     
 };
